4835e79124fbd704f47dddc6bf6c4fd24f9c82eb
Yoonsung Ahn <Yoonsung Ahn>
2016-02-28 09:40:52 -0800

    more htings in search

--- BookMarket/app/src/main/java/com/bookmarketer/nw/bookmarket/MainMenu.java
+++ BookMarket/app/src/main/java/com/bookmarketer/nw/bookmarket/MainMenu.java
@@ -1,10 +1,14 @@
 package com.bookmarketer.nw.bookmarket;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -12,7 +16,10 @@ public class MainMenu extends AppCompatActivity {
     private Button mActiveBtn;
     private Button mlogOutBtn;
     private Button mChatBtn;
+    private EditText mSearchText;
+    private final String SERVER_URL="http://rafaytanzeel.com/bookMarket/bookMethods.php";
 
+    private Button mSearchSubmitBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,8 @@ public class MainMenu extends AppCompatActivity {
         mActiveBtn = (Button) findViewById(R.id.activeBtn);
         mlogOutBtn = (Button) findViewById(R.id.logoutBtn);
         mChatBtn = (Button) findViewById(R.id.chatBtn);
+        mSearchText = (EditText) findViewById(R.id.searchText);
+        mSearchSubmitBtn = (Button) findViewById(R.id.searchSubmitBtn);
         mUploadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,6 +60,27 @@ public class MainMenu extends AppCompatActivity {
                 finish();
             }
         });
-
+        mSearchSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RequestPackage p =new RequestPackage();
+                p.setMethod("POST");
+                p.setUri(SERVER_URL);
+                p.setParam("method", "search");
+                p.setParam("title", mSearchText.getText().toString());
+                sendSearchPost userDatePost=new sendSearchPost();
+                userDatePost.execute(p);
+            }
+        });
     }
+}
+class sendSearchPost extends AsyncTask<RequestPackage, Void, Void> {
+
+    @Override
+    protected Void doInBackground(RequestPackage... params) {
+        String jsonData = httpManager.getData(params[0]);
+        Log.d("JSON", jsonData);
+        return null;
+    }
+
 }
