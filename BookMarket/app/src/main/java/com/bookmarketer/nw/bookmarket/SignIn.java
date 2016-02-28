737a14901be8a25f957f745aef4172b5081ef213
Rafay Tanzeel <rafaytanzeel@gmail.com>
2016-02-27 17:17:32 -0800

    raw creation of signin

--- BookMarket/app/src/main/java/com/bookmarketer/nw/bookmarket/SignIn.java
+++ BookMarket/app/src/main/java/com/bookmarketer/nw/bookmarket/SignIn.java
@@ -1,21 +1,199 @@
 package com.bookmarketer.nw.bookmarket;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignIn extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+public class SignIn extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
+
+    private GoogleApiClient mGoogleApiClient;
+    private TextView mStatus;
+    private ProgressDialog mProgressDialog;
+    private Button signIn;
+    private Button signOut;
+    private Button disconnect;
+    private Button proceed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestProfile()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        mStatus = (TextView) findViewById(R.id.status);
+        signIn = (Button) findViewById(R.id.sign_in_button);
+        signOut = (Button) findViewById(R.id.sign_out_button);
+        disconnect = (Button) findViewById(R.id.disconnect_button);
+        proceed = (Button) findViewById(R.id.nextPage);
+
+        signIn.setOnClickListener(this);
+        signOut.setOnClickListener(this);
+        disconnect.setOnClickListener(this);
+        proceed.setOnClickListener(this);
+//        signIn.setSize(SignInButton.SIZE_STANDARD);
+//        signIn.setScopes(gso.getScopeArray());
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            Toast.makeText(this, "Got cached sign-in", Toast.LENGTH_LONG).show();
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            showProgressDialog();
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(GoogleSignInResult googleSignInResult) {
+                    Toast.makeText(SignIn.this, "Got in onStart", Toast.LENGTH_LONG).show();
+                    hideProgressDialog();
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
     }
 
 
+    private void signIn() {
+        Toast.makeText(this, "Pressed signed in button", Toast.LENGTH_LONG).show();
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
-    public void move(View view){
+    private void signOut() {
+        Toast.makeText(this, "Pressed signed out button", Toast.LENGTH_LONG).show();
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        updateUI(false);
+                    }
+                }
+        );
+    }
+
+    private void revokeAccess() {
+        Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        updateUI(false);
+                    }
+                }
+        );
+    }
+
+
+    private void updateUI(boolean signedIn) {
+        if (signedIn) {
+            signIn.setVisibility(View.GONE);
+            signOut.setVisibility(View.VISIBLE);
+            disconnect.setVisibility(View.VISIBLE);
+            proceed.setVisibility(View.VISIBLE);
+        } else {
+            mStatus.setText("Signed In Please");
+            signIn.setVisibility(View.VISIBLE);
+            signOut.setVisibility(View.GONE);
+            disconnect.setVisibility(View.GONE);
+            proceed.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            GoogleSignInAccount account = result.getSignInAccount();
+            mStatus.setText(account.getDisplayName() + "is Signed In");
+            updateUI(true);
+        } else {
+            updateUI(false);
+        }
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(true);
+        }
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Please Connect to Wifi", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                Toast.makeText(this, "Pressed signed in button", Toast.LENGTH_LONG).show();
+                signIn();
+                break;
+            case R.id.sign_out_button:
+                signOut();
+                break;
+            case R.id.disconnect_button:
+                revokeAccess();
+                break;
+            case R.id.nextPage:
+                goToNextPage();
+                break;
+        }
+    }
+
+
+    private void goToNextPage() {
         Intent i = new Intent(this, MainMenu.class);
         startActivity(i);
     }
